4bf49884b23a3ba8f425486e93a385d000ed60f0
Caleb McQuillin <codebycaleb@users.noreply.github.com>
2024-09-04 08:33:48 -0400

    Script changes

--- .codecrafters/run.sh
+++ .codecrafters/run.sh
@@ -10,4 +10,4 @@ set -e # Exit on failure
 set -o pipefail # Prevent masking pipe failures
 
 # TODO: Use --no-print-progress once https://github.com/gleam-lang/gleam/issues/2299 is implemented
-exec gleam run --module lox -- "$@" | grep -v "Compiled in" | grep -v "Running lox.main"
+exec gleam run --module lox -- "$@" | { grep -v "Compiled in" || true; } | { grep -v "Running lox.main" || true; }
--- lox.sh
+++ lox.sh
@@ -5,21 +5,8 @@
 # Note: Changing this script WILL NOT affect how CodeCrafters runs your program.
 #
 # Learn more: https://codecrafters.io/program-interface
-
-set -e # Exit early if any commands fail
+set -e # Exit on failure
 set -o pipefail # Prevent masking pipe failures
 
-# Copied from .codecrafters/compile.sh
-#
-# - Edit this to change how your program compiles locally
-# - Edit .codecrafters/compile.sh to change how your program compiles remotely
-(
-  cd "$(dirname "$0")" # Ensure compile steps are run within the repository directory
-  gleam build
-)
-
-# Copied from .codecrafters/run.sh
-#
-# - Edit this to change how your program runs locally
-# - Edit .codecrafters/run.sh to change how your program runs remotely
-exec gleam run --module lox -- "$@" | grep -v "Compiled in" | grep -v "Running lox.main"
+# TODO: Use --no-print-progress once https://github.com/gleam-lang/gleam/issues/2299 is implemented
+exec gleam run --module lox -- "$@" | { grep -v "Compiled in" || true; } | { grep -v "Running lox.main" || true; }
